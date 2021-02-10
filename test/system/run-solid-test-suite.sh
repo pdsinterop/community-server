12053f973683fcba14cb0b5653128522841c494a
Michiel de Jong <michiel@unhosted.org>
2021-02-10 11:57:01 +0100

    Don't skip wps

--- test/system/run-solid-test-suite.sh
+++ test/system/run-solid-test-suite.sh
@@ -21,7 +21,7 @@ npm ci
 export SERVER_ROOT=http://localhost:3000/
 export ALICE_WEBID_DOC=${SERVER_ROOT}profile.ttl
 export ALICE_WEBID=$ALICE_WEBID_DOC#me
-export SKIP_WPS=1
+# export SKIP_WPS=1
 curl -X PUT $ALICE_WEBID_DOC -H 'Content-Type: text/turtle' -d "<$ALICE_WEBID> <http://www.w3.org/ns/pim/space#storage> </>."
 npm run jest
 
