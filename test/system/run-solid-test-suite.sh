b5fd8796dfea55b639edd96465e70c7399c57b00
Michiel de Jong <michiel@unhosted.org>
2021-02-10 13:44:38 +0100

    Fix slash

--- test/system/run-solid-test-suite.sh
+++ test/system/run-solid-test-suite.sh
@@ -21,7 +21,7 @@ npm ci
 
 # Run tests
 export SERVER_ROOT=http://localhost:3000
-export ALICE_WEBID_DOC=${SERVER_ROOT}profile.ttl
+export ALICE_WEBID_DOC=${SERVER_ROOT}/profile.ttl
 export ALICE_WEBID=$ALICE_WEBID_DOC#me
 curl -X PUT $ALICE_WEBID_DOC -H 'Content-Type: text/turtle' -d "<$ALICE_WEBID> <http://www.w3.org/ns/pim/space#storage> </>."
 npm run jest
