7e1db8b952426c6c7bc470dc2cb5fde22d810f9a
Michiel de Jong <michiel@unhosted.org>
2021-02-10 13:24:45 +0100

    remove trailing slash in SERVER_ROOT

--- test/system/run-solid-test-suite.sh
+++ test/system/run-solid-test-suite.sh
@@ -15,13 +15,14 @@ cd solid-crud-tests
 # Please uncomment before committing to master:
 # git checkout v2.0.3
 git checkout css-tests
+git pull
+git status
 npm ci
 
 # Run tests
-export SERVER_ROOT=http://localhost:3000/
+export SERVER_ROOT=http://localhost:3000
 export ALICE_WEBID_DOC=${SERVER_ROOT}profile.ttl
 export ALICE_WEBID=$ALICE_WEBID_DOC#me
-# export SKIP_WPS=1
 curl -X PUT $ALICE_WEBID_DOC -H 'Content-Type: text/turtle' -d "<$ALICE_WEBID> <http://www.w3.org/ns/pim/space#storage> </>."
 npm run jest
 
