c8c1f4704a9c3cdbf090716eb4d867adbbc3f7aa
MohammadReza vahedi <mr.vahedi68@gmail.com>
2023-09-02 14:46:56 +0000

    remove docker from tests
    Signed-off-by: MohammadReza vahedi <mr.vahedi68@gmail.com>

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -2,62 +2,48 @@
 set -e
 
 function setup {
-  echo Branch name: $1
-  docker network create testnet
-  docker build -t server .
-  docker build -t cookie test/surface/docker/cookie
-  docker run -d --env-file test/surface/server-env.list --name server --network=testnet server
-  docker run -d --env-file test/surface/thirdparty-env.list --name thirdparty --network=testnet server
-}
-function teardown {
-  docker stop `docker ps --filter network=testnet -q`
-  docker rm `docker ps --filter network=testnet -qa`
-  docker network remove testnet
+  npm ci
+  npm start &> '/dev/null' &
+  git clone https://github.com/solid-contrib/solid-crud-tests.git
+  cd solid-crud-tests
+  git fetch origin
+  git checkout nss-skips
+  git pull
+  npm ci
+  cd ..
+  git clone https://github.com/solid-contrib/web-access-control-tests.git
+  cd  web-access-control-tests
+  #git fetch origin
+  #git checkout run-against-css
+  git pull
+  npm ci
+  cd ..
 }
 
-function waitForNss {
-  docker pull solidtestsuite/webid-provider-tests
-  until docker run --rm --network=testnet solidtestsuite/webid-provider-tests curl -kI http://$1:3000 2> /dev/null > /dev/null
+function waitForCss {
+  until curl -kI http://localhost:3000 2> /dev/null
   do
-    echo Waiting for $1 to start, this can take up to a minute ...
-    docker ps -a
-    docker logs $1
+    echo Waiting for CSS to start, this can take up to a minute ...
     sleep 1
   done
+}
 
-  docker logs $1
-  echo Getting cookie for $1...
-  export COOKIE_$1="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file test/surface/$1-env.list cookie`"
+function teardown {
+  kill $(lsof -t -i :3000)
+  rm -rf solid-crud-tests web-access-control-tests
 }
 
 function runTests {
-  docker pull solidtestsuite/$1:$2
-  
-  echo "Running $1 against server with cookie $COOKIE_server"
-  docker run --rm --network=testnet \
-    --env COOKIE="$COOKIE_server" \
-    --env COOKIE_ALICE="$COOKIE_server" \
-    --env COOKIE_BOB="$COOKIE_thirdparty" \
-    --env-file test/surface/$1-env.list solidtestsuite/$1:$2
+  cd $1
+  ./run-against-css.sh
+  cd ..
 }
 
 # ...
 teardown || true
-setup $1
-waitForNss server
-#runTests webid-provider-tests v2.0.3
-runTests solid-crud-tests v6.0.0
-#waitForNss thirdparty
-#runTests web-access-control-tests v7.1.0
+setup
+waitForCss
+runTests solid-crud-tests
+runTests web-access-control-tests
 teardown
-
-# To debug, e.g. running web-access-control-tests jest interactively,
-# comment out `teardown` and uncomment this instead:
-# env
-# docker run -it --network=testnet \
-#     --env COOKIE="$COOKIE_server" \
-#     --env COOKIE_ALICE="$COOKIE_server" \
-#     --env COOKIE_BOB="$COOKIE_thirdparty" \
-#     --env-file test/surface/web-access-control-tests-env.list \
-#   solidtestsuite/web-access-control-tests:latest /bin/bash
 
